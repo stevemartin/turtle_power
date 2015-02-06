6e77e6210f9e6f6499e5691e315e5e148cce408f
Maciek Paliński <maciekpalinski@gmail.com>
2015-02-06 10:17:48 +0000

    you're a turtle

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
 define(['d3.v3', 'elasticsearch.min'], function (d3, elasticsearch) {
   "use strict";
-  var client = new elasticsearch.Client({
-    host: 'qa14.hq.noths.com:9200',
-    log: 'trace'
-  });
+
+  var serverAddress = 'qa14.hq.noths.com:9200';
+  var ordersIndex = 'orders_uk_staging_20150204105930';
+  var client = new elasticsearch.Client({ host: serverAddress });
 
   client.search({
-    index: 'orders_uk_staging_20150204105930',
-        size: 0,
-        body: {
-          // Begin query.
-          query: {
-            term: { 'product_ids': 245040 }
-          },
-          // Aggregate on the results
-          aggs: {
-            range: {
-              date_histogram: {
-                field: 'placed_at',
-                interval: 'day'
-              }
-            }
+    index: ordersIndex,
+    size: 0,
+    body: {
+      query: {
+        term: { 'product_ids': 245040 }
+      },
+      aggs: {
+        range: {
+          date_histogram: {
+            field: 'placed_at',
+            interval: 'day'
           }
         }
+      }
+    }
   }).then(function(resp){
     console.log(resp);
   });
-
 });
